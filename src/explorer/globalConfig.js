b8c28109df6a4b9a79d63886cd2862b45cee4346
Nordic Energy <54575925+nordicenergy@users.noreply.github.com>
2021-03-22 11:48:24 +0200

    Update globalConfig.js

--- src/explorer/globalConfig.js
+++ src/explorer/globalConfig.js
@@ -6,10 +6,10 @@ export const EXPLORER_BACKEND_WS = process.env.VUE_APP_EXPLORER_BACKEND_WS
 export const SECRET = process.env.SECRET
 
 export const BASE_HRC20URL =
-  'https://raw.githubusercontent.com/harmony-one/HRC20-logos/master'
-//export const BASE_HRC20URL ='https://raw.githubusercontent.com/harmony-one/HRC20-logos/testnet';
+  'https://raw.githubusercontent.com/nordcienergy.io/HRC20-logos/master'
+//export const BASE_HRC20URL ='https://raw.githubusercontent.com/nordicenergy/HRC20-logos/testnet';
 export const HRC20_HOLDERURL =
   'https://harmony-hrc-holder.firebaseapp.com/#/address'
-export const ONE_HOLDERURL = 'https://balance.harmony.one'
+export const ONE_HOLDERURL = 'https://balance.nordicenergy.io'
 
 export const SDK_NODE = process.env.VUE_APP_SDK_NODE
