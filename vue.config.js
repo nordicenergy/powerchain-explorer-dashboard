253df46e44998c9c133cc393b2c0dd9f6c577db7
Nordic Energy Ltd <5032986-nordicenergy@users.noreply.gitlab.com>
2021-03-22 12:43:42 +0200

    first commit

--- vue.config.js
+++
@@ -1,9 +0,0 @@
-const isProduction = process.env.NODE_ENV === 'production'
-const CompressionPlugin = require('compression-webpack-plugin')
-module.exports = {
-  productionSourceMap: isProduction ? false : true,
-  chainWebpack(config) {
-    if (!isProduction) return
-    config.plugin('CompressionPlugin').use(CompressionPlugin)
-  },
-}
